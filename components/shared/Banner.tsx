a4361627ccacd17df7a60441419335060d80ef5e
Syed Adeeb <adeeb@Syeds-MacBook-Pro.local>
2024-01-19 15:53:32 +0530

    Day 15: Banner UI Update.

--- components/shared/Banner.tsx
+++ components/shared/Banner.tsx
@@ -8,6 +8,7 @@ import {
 	CarouselItem,
 } from "@/components/ui/carousel";
 import Autoplay from "embla-carousel-autoplay";
+import Link from "next/link";
 
 type Props = {};
 
@@ -16,30 +17,56 @@ const Banner = (props: Props) => {
 		Autoplay({ delay: 4000, stopOnInteraction: false })
 	);
 	return (
-		<section className="text-gray-200 body-font w-full min-h-[94vh]">
-			<div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col lg:flex-row">
-				<div className="relative md:h-[800px] h-[500px] flex items-center justify-center">
+		<section className="text-gray-200 body-font w-full min-h-[94vh] relative">
+			<div className="absolute bgImg h-[94vh] top-20 left-[50%] -translate-x-[40%] -z-0 " />
+			<div className="container mx-auto flex px-5 py-24 items-center justify-center flex-col lg:flex-row blur-0 w-full">
+				<div className="relative md:h-[800px] mt-[100px] flex items-center justify-center">
 					<Carousel
 						plugins={[plugin.current]}
 						orientation="horizontal"
 						className="w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] md:w-[600px] md:h-[600px]
-					lg:w-[700px] lg:h-[700px]"
-					>
+					lg:w-[700px] lg:h-[700px]">
 						<CarouselContent>
 							<CarouselItem>
 								<div
-									className="relative w-[300px] h-[300px] sm:w-[300px] sm:h-[300px] md:w-[500px] md:h-[500px]
-					lg:w-[600px] lg:h-[600px] object-contain md:mx-20 filter"
-								>
+									className="relative w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] md:w-[500px] md:h-[500px]
+					lg:w-[600px] lg:h-[600px] object-contain md:mx-20 filter ">
 									<Image src="/banner-1.webp" alt="" layout="fill" />
 								</div>
 							</CarouselItem>
 							<CarouselItem>
 								<div
-									className="relative w-[300px] h-[300px] sm:w-[300px] sm:h-[300px] md:w-[500px] md:h-[500px]
-					lg:w-[600px] lg:h-[600px] object-contain md:mx-20 filter"
-								>
+									className="relative w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] md:w-[500px] md:h-[500px]
+					lg:w-[600px] lg:h-[600px] object-contain md:mx-20 filter ">
+									<Image src="/banner.png" alt="" layout="fill" />
+								</div>
+							</CarouselItem>
+							<CarouselItem>
+								<div
+									className="relative w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] md:w-[500px] md:h-[500px]
+					lg:w-[700px] lg:h-[650px] object-contain filter ">
+									<Image src="/banner-1-fb.webp" alt="" layout="fill" />
+								</div>
+							</CarouselItem>
+							<CarouselItem>
+								<div
+									className="relative w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] md:w-[500px] md:h-[500px]
+					lg:w-[600px] lg:h-[600px] object-contain md:mx-20 filter ">
 									<Image src="/banner-1-1.webp" alt="" layout="fill" />
+								</div>
+							</CarouselItem>
+							<CarouselItem>
+								<div
+									className="relative w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] md:w-[500px] md:h-[500px]
+					lg:w-[600px] lg:h-[600px] object-contain md:mx-20 filter ">
+									<Image src="/banner-2-HD.png" alt="" layout="fill" />
+								</div>
+							</CarouselItem>
+							<CarouselItem>
+								<div
+									className="relative w-[300px] h-[300px] sm:w-[400px] sm:h-[400px] md:w-[500px] md:h-[500px]
+					lg:w-[700px] lg:h-[650px] object-contain filter ">
+									<Image src="/banner-2-2.webp" alt="" layout="fill" />
 								</div>
 							</CarouselItem>
 						</CarouselContent>
@@ -50,18 +77,29 @@ const Banner = (props: Props) => {
 					<div className="absolute bg-gradient-to-r from-transparent z-10 to-[#1b1b1b] w-20  h-[100%] right-0" /> */}
 				</div>
 				<div className="text-center lg:text-left lg:w-2/3 w-full">
-					<p className="mt-4 leading-relaxed text-white text-2xl">
+					<Image
+						src="/oneplus-logo.webp"
+						alt=""
+						width={300}
+						height={70}
+						className="animate-pulse"
+					/>
+					<p className="mt-4 leading-relaxed text-gray-400 text-2xl">
 						Never Settle
 					</p>
-					<h1 className="title-font lg:text-5xl text-3xl font-medium text-gray-300">
+					<h1 className="title-font lg:text-5xl text-3xl font-medium text-gray-100">
 						One Plus Nord CE 3 Lite 5G
 					</h1>
-					<p className="mb-4 leading-relaxed text-white text-4xl">
-						Starting at ₹29,999
+					<p className="mb-4 leading-relaxed text-gray-300 text-4xl">
+						Starting at ₹19,999
 					</p>
 					<div className="flex lg:justify-start justify-center space-x-4">
-						<Button variant="default">View Product</Button>
-						<Button variant="default">View Store</Button>
+						<Link href={"/mobiles/65a8a42692b38549cce8b1c9"}>
+							<Button variant="default">View Product</Button>
+						</Link>
+						<Link href={"/store?page=1"}>
+							<Button variant="default">View Store</Button>
+						</Link>
 					</div>
 				</div>
 			</div>
